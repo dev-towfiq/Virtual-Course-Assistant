0a75617c6448cd2171e93151048ebcd080ee753d
dev-towfiq <contact@towfiqalahesohel.com>
2019-12-13 19:49:51 +0600

    V1.2

--- app/src/main/java/com/toufiq/virtualCourseAssistant/MainActivity.java
+++ app/src/main/java/com/toufiq/virtualCourseAssistant/MainActivity.java
@@ -1,7 +1,6 @@
 package com.toufiq.virtualCourseAssistant;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -11,75 +10,34 @@ import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.text.format.DateUtils;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
     private TextToSpeech tts;
-    private SpeechRecognizer speechRecog;
-
-    // push
+    private SpeechRecognizer speechRecognizer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Here, thisActivity is the current activity
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.RECORD_AUDIO)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-                    // Permission is not granted
-                    // Should we show an explanation?
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                            Manifest.permission.RECORD_AUDIO)) {
-                        // Show an explanation to the user *asynchronously* -- don't block
-                        // this thread waiting for the user's response! After the user
-                        // sees the explanation, try again to request the permission.
-                    } else {
-                        // No explanation needed; request the permission
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
-
-                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                        // app-defined int constant. The callback method gets the
-                        // result of the request.
-                    }
-                } else {
-                    // Permission has already been granted
-                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                    intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 20);
-                    speechRecog.startListening(intent);
-                }
-            }
-        });
-
         initializeTextToSpeech();
         initializeSpeechRecognizer();
     }
-
+    public void fab(View view) {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 20);
+            speechRecognizer.startListening(intent);
+    }
     private void initializeSpeechRecognizer() {
         if (SpeechRecognizer.isRecognitionAvailable(this)) {
-            speechRecog = SpeechRecognizer.createSpeechRecognizer(this);
-            speechRecog.setRecognitionListener(new RecognitionListener() {
+            speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+            speechRecognizer.setRecognitionListener(new RecognitionListener() {
                 @Override
                 public void onReadyForSpeech(Bundle params) {
 
@@ -129,85 +87,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @SuppressLint("MissingPermission")
-    private void processResult(String result_message) {
-        result_message = result_message.toLowerCase();
-
-//        Handle at least four sample cases
-
-//        First: What is your Name?
-//        Second: What is the time?
-//        Third: Is the earth flat or a sphere?
-//        Fourth: Open a browser and open url
-
-        if (result_message.contains("what")) {
-
-            if (result_message.contains("your name")) {
-                speak("My Name is Kitto");
-            }
-            if (result_message.contains("software")) {
-
-                if (result_message.contains("engineering")) {
-                    speak("Software engineering is the application of engineering to the design, development, implementation and maintenance of software in a systematic method");
-                } else {
-                    speak("Software is a general term for the various kinds of programs used to operate computers. And related devices with all its associated documents and configuration data. For example," +
-                            "without your Internet browser, you could not surf the Internet or read this page. Without an operating system, the browser could not run on your computer" + "Like me, i'm a software. And Towfiq created me");
-                }
-
-            }
-        }
-
-        if (result_message.contains("open lecture 1")) {
-            Intent intent = new Intent(this, ActivityShowLecture.class);
-            startActivity(intent);
-        }
-        if (result_message.contains("our team info")) {
-            Intent intent = new Intent(this, ActivityAboutTeam.class);
-            startActivity(intent);
-        }
-
-        if (result_message.contains("my faculty info")) {
-            Intent intent = new Intent(this, ActivityAboutFaculty.class);
-            startActivity(intent);
-        }
-        if (result_message.contains("call my faculty")) {
-            Intent intent = new Intent(Intent.ACTION_CALL);
-            intent.setData(Uri.parse("tel:001087654321"));
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            startActivity(intent);
-        }
-        if (result_message.contains("email my faculty")){
-            Intent intent = new Intent(Intent.ACTION_SENDTO);
-            intent.setData(Uri.parse("mailto:@khan.habibullah@northsouth.edu"));
-            startActivity(intent);
-        }
-
-
-         if (result_message.contains("who")){
-            if(result_message.contains("you")){
-                speak("I'm Kitto. I'm a your cse 3 2 7 course assistant. I am developed by Towfiq");
-
-            }
-        }
-    }
-
-    private void initializeTextToSpeech() {
-        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (tts.getEngines().size() == 0 ){
-                    Toast.makeText(MainActivity.this, getString(R.string.tts_no_engines),Toast.LENGTH_LONG).show();
-                    finish();
-                } else {
-                    tts.setLanguage(Locale.US);
-                    speak("Hi, I'm your CSE 3 2 7 course assistant. You can ask me any question regarding this course");
-                }
-            }
-        });
-    }
-
     private void speak(String message) {
         if(Build.VERSION.SDK_INT >= 21){
             tts.speak(message,TextToSpeech.QUEUE_FLUSH,null,null);
@@ -215,14 +94,70 @@ public class MainActivity extends AppCompatActivity {
             tts.speak(message, TextToSpeech.QUEUE_FLUSH,null);
         }
     }
+//        Command 1: who are you?
+//        Command 2: my faculty info
+//        Command 3: call my faculty
+//        Command 4: email my faculty
+//        Command 5: what is software?
+//        Command 6: what is software engineering?
+//        Command 7: open lecture 1
+//        Command 8: our team info
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void processResult(String command) {
+        command = command.toLowerCase();
+        if (command.contains("who")){
+            if(command.contains("you")){
+                speak(getString(R.string.who_are_your));
+            }
+        }
+        if (command.contains("what")) {
+            if (command.contains("your name")) {
+                speak(getString(R.string.what_is_your_name));
+            }
+            if (command.contains("software")) {
+                if (command.contains("engineering")) {
+                    speak(getString(R.string.software_engineering));
+                } else {
+                    speak(getString(R.string.software));
+                }
+            }
+        }
+        if (command.contains("open lecture 1")) {
+            Intent intent = new Intent(this, ActivityShowLecture.class);
+            startActivity(intent);
+        }
+        if (command.contains("our team info")) {
+            Intent intent = new Intent(this, ActivityAboutTeam.class);
+            startActivity(intent);
+        }
+        if (command.contains("my faculty info")) {
+            Intent intent = new Intent(this, ActivityAboutFaculty.class);
+            startActivity(intent);
+        }
+        if (command.contains("call my faculty")) {
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:001087654321"));
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            startActivity(intent);
+        }
+        if (command.contains("email my faculty")){
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:khan.habibullah@northsouth.edu"));
+            startActivity(intent);
+        }
     }
-
+    private void initializeTextToSpeech() {
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                    tts.setLanguage(Locale.US);
+                    speak(getString(R.string.who_is_Lalita));
+            }
+        });
+    }
+    //activity lifecycle
     @Override
     protected void onPause() {
         super.onPause();
@@ -232,29 +167,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-//        Reinitialize the recognizer and tts engines upon resuming from background such as after opening the browser
+        //Reinitialize the recognizer upon resuming from background
         initializeSpeechRecognizer();
-        initializeTextToSpeech();
     }
-
-    public void button1(View view) {
-        Intent intent= new Intent(this,ActivityAboutTeam.class);
-        startActivity(intent);
-    }
-
-    public void faculty(View view) {
-        Intent intent= new Intent(this,ActivityAboutFaculty.class);
-        startActivity(intent);
-    }
-
-
 }
-//        Q1: our team info
-//        Q2: my faculty info
-//        Q3: call my faculty
-//        Q4: email my faculty
-//        Q5: what is software?
-//        Q6: what is software engineering?
-//        Q7: who are you?
-//
-
